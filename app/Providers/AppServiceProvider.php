886fb05e1f1f70e4aded7341e97520cf1c3da08b
John Christian P. Oraa <51404378+johnchristianoraa@users.noreply.github.com>
2020-12-01 22:02:01 +0800

    updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \URL::forceScheme('https');
+
         Schema::defaultStringLength(191);
     }
 }
