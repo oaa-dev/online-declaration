dba9f7967ff9e6b86ea76412ffa5c7cd07c7bc69
John Christian P. Oraa <51404378+johnchristianoraa@users.noreply.github.com>
2020-12-13 10:27:36 +0800

    updated

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -144,7 +144,7 @@
     <ul class="navbar-nav ml-auto">
       <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" id="number" aria-expanded="true">
-          <i class="fa fa-bell-o"></i>
+          <i class="fa fa-bell"></i>
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right" style="left: inherit; right: 0px;" id="notifications">       
           <a href="/monitoring/health-status" class="dropdown-item dropdown-footer">See All</a>
