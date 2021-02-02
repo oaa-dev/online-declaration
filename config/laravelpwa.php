6da16deb17a4dc74764f75eb59e08246b1f64c84
John Christian P. Oraa <51404378+johnchristianoraa@users.noreply.github.com>
2021-02-02 21:11:58 +0800

    updated

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -13,40 +13,40 @@ return [
         'status_bar'=> 'black',
         'icons' => [
             '72x72' => [
-                'path' => '/images/icons/Forgems-MKTG.png',
+                'path' => '/images/icons/logo.png',
                 'purpose' => 'any'
             ],
             '96x96' => [
-                'path' => '/images/icons/Forgems-MKTG.png',
+                'path' => '/images/icons/logo.png',
                 'purpose' => 'any'
             ],
             '128x128' => [
-                'path' => '/images/icons/Forgems-MKTG.png',
+                'path' => '/images/icons/logo.png',
                 'purpose' => 'any'
             ],
             '144x144' => [
-                'path' => '/images/icons/Forgems-MKTG.png',
+                'path' => '/images/icons/logo.png',
                 'purpose' => 'any'
             ],
             '152x152' => [
-                'path' => '/images/icons/Forgems-MKTG.png',
+                'path' => '/images/icons/logo.png',
                 'purpose' => 'any'
             ],
             '192x192' => [
-                'path' => '/images/icons/Forgems-MKTG.png',
+                'path' => '/images/icons/logo.png',
                 'purpose' => 'any'
             ],
             '384x384' => [
-                'path' => '/images/icons/Forgems-MKTG.png',
+                'path' => '/images/icons/logo.png',
                 'purpose' => 'any'
             ],
             '512x512' => [
-                'path' => '/images/icons/Forgems-MKTG.png',
+                'path' => '/images/icons/logo.png',
                 'purpose' => 'any'
             ],
         ],
         'splash' => [
-            '640x1136' => '/images/icons/Forgems-MKTG.png',
+            '640x1136' => '/images/icons/logo.png',
             '750x1334' => '/images/icons/Forgems-MKTG.png',
             '828x1792' => '/images/icons/Forgems-MKTG.png',
             '1125x2436' => '/images/icons/Forgems-MKTG.png',
